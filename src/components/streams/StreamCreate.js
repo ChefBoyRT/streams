b778e086b34346c7a812ab87c2d412b280327fc2
Taylor <stein0209@gmail.com>
2019-12-01 15:27:16 -0500

    first commit

---
+++ src/components/streams/StreamCreate.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamCreate extends Component {
+    render(){
+        return (
+            <div>
+                <form>
+                    <Field name="title" />
+                    <Field name="description" />
+                </form>
+            </div>
+        )
+    };
+}
+
+export default reduxForm({ form: 'streamCreate' })(StreamCreate);
